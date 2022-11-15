a6e267de869deadbb45efc8d951a4678de7902f3
Isac <mr_izac@hotmail.com>
2022-11-15 13:57:35 +0100

    script script :)

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,14 @@
   window.addEventListener("beforeunload", function () {
   document.body.classList.add("animate-out");
   });
+
+// View all button onclick
+
+function onclickfunktion() {
+  const viewallbtn = document.getElementsByClassName('container-2');
+
+  for(let i = 0; i < viewallbtn.length; i++) {
+          viewallbtn.item(i).classList.toggle("show");
+    }
+
+}
